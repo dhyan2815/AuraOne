21ad23d00a8fea53a4d7ffe920043ae1c0f32aa9
dhyan2815 <dhyan.work.2815@gmail.com>
2026-05-11 21:32:27 +0530

    fix: convert CommonJS require to ESM import for typography plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 import animate from 'tailwindcss-animate'
+import typography from '@tailwindcss/typography'
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -93,5 +94,5 @@ export default {
       },
     },
   },
-  plugins: [animate, require('@tailwindcss/typography')],
+  plugins: [animate, typography],
 };
